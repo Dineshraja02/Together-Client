efec0cf5f1221a9253478ef7e17286696069d95c
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-12 10:44:07 +0530

    warn

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -8,7 +8,6 @@ import axios from "axios";
 import { useParams } from "react-router";
 
 export default function Profile() {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [user, setUser] = useState({});
   const username = useParams().username;
 
